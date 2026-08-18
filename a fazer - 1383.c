f1e8b7f4dc10cd99bdd68a7d409cef71eca12bd4
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-08-18 15:09:40 -0300

    Update a fazer - 1383.c

--- a fazer - 1383.c
+++ a fazer - 1383.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
  
 int vcoluna (sudoku[9][9]) {
-           
+    for (int i=0;i<9;i++){
+         for (int j=0;j<9;j++){
+             for (int j2=j;j2<9;j2++){
+               
+             }
+         }
+     } 
 }
 
 int vlinha (sudoku[9][9]) {
+    for (int i=0;i<9;i++){
+         for (int j=0;j<9;j++){
+             scanf ("%d",&sudoku[i][j]);
+         }
+     }
         
 }
 
@@ -23,28 +34,34 @@ int main() {
     // Cria uma matriz com o sudoku.
     int sudoku[9][9];
     
-    int ncasos;//Quantas instâncias há.
+    int ncasos; //Quantas instâncias há.
     scanf ("%d"ncasos);
+
+    int instancia = 0; //QUantas instâncias há.
     
     // Uma variável que dita o resultado.
     int r=1;// "r" de resultado, por hora, verdade até que se prove o contrário.
     
     while (ncasos>0){
-    //Primeio, lê todas as colunas de uma mesma linha.
-    for (int i=0;i<9;i++){
-        for (int j=0;j<9;j++){
-            scanf ("%d",&sudoku[i][j]);
-        }
-    }
-    
-    r = vlinha;
-    resultado (instancia,r);
-    r = vcoluna;
-    resultado (instancia,r);
-    r = vbloco;
-    resultado (instancia,r);
-    
-    prinf("Instancia %d\nSIM",instancia);
- 
-    return 0;
+     instancia ++; // Cada laço é uma instância.
+     
+     //Primeio, lê todas as colunas de uma mesma linha.
+     for (int i=0;i<9;i++){
+         for (int j=0;j<9;j++){
+             scanf ("%d",&sudoku[i][j]);
+         }
+     }
+     
+     r = vlinha;
+     resultado (instancia,r);
+     r = vcoluna;
+     resultado (instancia,r);
+     r = vbloco;
+     resultado (instancia,r);
+     
+     prinf("Instancia %d\nSIM",instancia);
+     
+     ncasos--; // Um caso já foi visto.
+   }
+     return 0;
 }
