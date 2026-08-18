ceeb8b4e993be198236e19f68121880589b84634
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-08-18 14:29:53 -0300

    Update and rename 1080.c to a fazer - 1080.c

--- 1080.c
+++
@@ -1,9 +0,0 @@
-/* --------------------------------------------------------------------------
-Disciplina  : Algoritmos e Estruturas de Dados I, turma IB, 2026S1
-Nome        : Gabriel Bernardes Cunha
-Linguagem   : C
-Problema    : https://judge.beecrowd.com/pt/problems/view/1013
-Data        : 19/08/2026
-Objetivo    : Calcular o maior de 3 numeros
-Aprendizado : Usar scanf, relizar contas, usar abs
--------------------------------------------------------------------------- */
