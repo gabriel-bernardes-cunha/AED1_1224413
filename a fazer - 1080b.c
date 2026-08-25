5b072b7b65725f1be8b0154c60c0987962857643
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-08-25 13:17:33 -0300

    Create a fazer - 1080b.c

---
+++ a fazer - 1080b.c
@@ -0,0 +1,37 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Gabriel Bernardes Cunha
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 25/08/2026
+Objetivo    : Realizar o exercício 1080 por alocação dinâmica.
+Dificuldade : 
+Uso de IA   : 
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main()
+{
+    int X[100], Maior, Posicao;
+
+    for (int i = 0; i < 100; i++)
+    {
+
+        scanf("%d", &X[i]);
+
+        if (i == 0)
+        {
+            Maior = X[i];
+            Posicao = i;
+        }
+
+        if (X[i] > Maior)
+        {
+            Maior = X[i];
+            Posicao = i;
+        }
+    }
+    printf("%d\n%d\n", Maior, Posicao + 1);
+
+    return 0;
+}
