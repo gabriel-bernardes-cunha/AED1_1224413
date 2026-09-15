2cb6aff8d19e65dc50cf6e1161d3e282f819f393
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-09-15 15:06:22 -0300

    Create to do 1340.c

---
+++ to do 1340.c
@@ -0,0 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Gabriel Bernardes Cunha
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1340
+Data        : 15/09/2026
+Objetivo    : 
+Dificuldade : 
+Uso de IA   : 
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+#include <stdlib.h>
