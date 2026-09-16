9f110cad16098e10b556eaa9380c4e328f8759e5
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-09-16 12:14:35 -0300

    Update to do 1340.c

--- to do 1340.c
+++ to do 1340.c
@@ -3,10 +3,77 @@ Disciplina  : Algortimo e Estrutura de Dados 2026S1
 Nome        : Gabriel Bernardes Cunha
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1340
-Data        : 15/09/2026
+Data        : 16/09/2026
 Objetivo    : 
-Dificuldade : 
+Dificuldade : Construir cada verificação. 
 Uso de IA   : 
 -------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <stdlib.h>
+
+int queue()
+{
+    if ()
+    { // Se for fila.
+        return 1;
+    }
+    else
+    { // Se não for fila.
+        return 0;
+    }
+}
+
+int stack()
+{
+    if ()
+    { // Se for pilha
+        return 2;
+    }
+    else
+    { // Se não for
+        return 0;
+    }
+}
+
+int priority_queue()
+{
+    if ()
+    { // Se for fila de prioridade.
+        return 3;
+    }
+    else
+    { // Se não for fila de prioridade.
+        return 0;
+    }
+}
+
+int main()
+{
+    int n;
+    while (scanf("%d", &n) != EOF)
+    {
+        int resultado = 0;
+        resultado = resultado + queue();
+        resultado = resultado + stack();
+        resultado = resultado + priority_queue();
+
+        switch (resultado)
+        {
+        case 0:
+            printf("impossible");
+
+        case 1:
+            printf("queue");
+
+        case 2:
+            printf("stack");
+
+        case 3:
+            printf("priority queue");
+
+        default:
+            printf("not sure");
+        }
+    }
+    return 0;
+}
