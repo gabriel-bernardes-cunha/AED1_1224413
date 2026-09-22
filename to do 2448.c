a7025128e783eafa81b0b0905b266432f3a67f2d
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-09-22 13:40:28 -0300

    Update to do 2448.c

--- to do 2448.c
+++ to do 2448.c
@@ -8,3 +8,25 @@ Objetivo    :
 Dificuldade : 
 Uso de IA   : 
 -------------------------------------------------------------------------- */
+//  N M, casas e encomendas
+//casas
+//ordem
+
+int main ()
+{
+  int N, M; // Casas e Encomendas. Não, não são a mesma coisa!
+  scanf ("%d, %d", &N, &M);
+
+  int casas[N], encomendas[M];
+  
+  for (int i = 0; i< N; i++)
+    {
+      scanf ("%d", &casas[i]);
+    }
+  
+  for (int i = 0; i < M; i++)
+    {
+      scanf ("%d", &encomendas[i]);
+    }
+  return 0;
+}
