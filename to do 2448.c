5544127c35be8583d1ab5947ef58e9156f6a4eb0
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-09-22 13:19:36 -0300

    Create to do 2448.c

---
+++ to do 2448.c
@@ -0,0 +1,10 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Gabriel Bernardes Cunha
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/2448
+Data        : 26/09/2026
+Objetivo    : 
+Dificuldade : 
+Uso de IA   : 
+-------------------------------------------------------------------------- */
