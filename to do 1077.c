01867dc330f1d687fa73e9f75f6f99336e59d682
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-09-24 12:23:07 -0300

    Create to do 1077.c

---
+++ to do 1077.c
@@ -0,0 +1,11 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Gabriel Bernardes Cunha
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1077
+Data        : 26/09/2026
+Objetivo    : 
+Dificuldade : 
+Uso de IA   :
+-------------------------------------------------------------------------- */
+#include <stdio.h>
