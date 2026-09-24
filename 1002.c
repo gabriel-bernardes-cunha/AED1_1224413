e30e775c65b976d83f52347f5d8aad6b94dbceab
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-09-24 13:55:25 -0300

    Update 1002.c

--- 1002.c
+++ 1002.c
@@ -4,7 +4,19 @@ Nome        : Gabriel Bernardes Cunha
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1002
 Data        : 24/09/2026
-Objetivo    : 
-Dificuldade : 
+Objetivo    : Escrever a área de um círculo
+Dificuldade : Conciliar os decimais
 Uso de IA   : 
 -------------------------------------------------------------------------- */
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+  double raio;
+  scanf ("%lf",&raio);
+
+  printf ("A=%0.4lf\n",3.14159*raio*raio);
+  
+  return 0;
+}
